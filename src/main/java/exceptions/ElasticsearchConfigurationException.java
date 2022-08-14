7992d9cdf938e8c451ceaf8d8d693e2d989071ec
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-08-14 20:19:19 +0200

    Added custom exceptions

---
+++ src/main/java/exceptions/ElasticsearchConfigurationException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+import helio.blueprints.exceptions.ActionException;
+
+/**
+ * Exception defined when there is an error in configuration.
+ *
+ * @author Emilio Crespo Perán
+ */
+public class ElasticsearchConfigurationException extends ActionException {
+
+	public ElasticsearchConfigurationException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ src/main/java/exceptions/ElasticsearchException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+import helio.blueprints.exceptions.ActionException;
+
+/**
+ * Exception defined when there is an error on performing an Elasticsearch operation.
+ *
+ * @author Emilio Crespo Perán
+ */
+public class ElasticsearchException extends ActionException {
+
+	public ElasticsearchException(String msg) {
+		super(msg);
+	}
+
+}
