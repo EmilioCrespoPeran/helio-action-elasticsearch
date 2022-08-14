7de59aa02f375c660f7dff8b8e56a52ce6ce5b28
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-08-14 21:41:33 +0200

    Create ElasticsearchCreateTests.java

---
+++ src/test/java/service/ElasticsearchCreateTests.java
@@ -0,0 +1,38 @@
+package service;
+
+import exceptions.ElasticsearchException;
+import model.ElasticsearchParameters;
+import org.junit.Test;
+import utils.MockupModel;
+import utils.TestUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class ElasticsearchCreateTests {
+
+    private ElasticsearchService service = new ElasticsearchService();
+
+    @Test
+    public void test01_CreateSimpleData() throws ElasticsearchException {
+        ElasticsearchParameters parameters = TestUtils.createParameters();
+        MockupModel model = TestUtils.createModel();
+
+        String obtained = service.create(parameters, TestUtils.toJson(model));
+        String expected = "created";
+
+        assertEquals(expected, obtained);
+    }
+
+    @Test
+    public void test02_CreateDataWithoutAllFields() throws ElasticsearchException {
+        ElasticsearchParameters parameters = TestUtils.createParameters();
+        MockupModel model = TestUtils.createModel();
+        model.setAge(null);
+
+        String obtained = service.create(parameters, TestUtils.toJson(model));
+        String expected = "created";
+
+        assertEquals(expected, obtained);
+    }
+
+}
